04dc4b72e18a138d8b59e5ec24e1b605b497f3d6
Santhosh Kumar Srinivasan <san@sanspace.in>
2017-06-22 15:29:02 -0400

    switch green and red icons
    This is to denote current page's security level the right way. There's
    been a long coversations going on in issue #6. This is a temp patch
    until we find a good set of icons

--- background.js
+++ background.js
@@ -19,13 +19,13 @@ function toggleHttps(tab) {
   
   function gotTitle(title) {
     if (title === TITLE_APPLY) {
-      browser.pageAction.setIcon({tabId: tab.id, path: "icons/padlock_red.svg"});
+      browser.pageAction.setIcon({tabId: tab.id, path: "icons/padlock_green.svg"});
       browser.pageAction.setTitle({tabId: tab.id, title: TITLE_REMOVE});
       var new_url = tab.url.replace(/http:/,'https:');
       var updating = browser.tabs.update({url: new_url});
       updating.then(onUpdated, onError);
     } else {
-      browser.pageAction.setIcon({tabId: tab.id, path: "icons/padlock_green.svg"});
+      browser.pageAction.setIcon({tabId: tab.id, path: "icons/padlock_red.svg"});
       browser.pageAction.setTitle({tabId: tab.id, title: TITLE_APPLY});
       var new_url = tab.url.replace(/https:/, 'http:');
       var updating = browser.tabs.update({url: new_url});
@@ -42,12 +42,12 @@ Initialize the page action: set icon and title, then show.
 */
 function initializePageAction(tab) {
   if (tab.url.startsWith('http:')) {
-    browser.pageAction.setIcon({tabId: tab.id, path: "icons/padlock_green.svg"});
+    browser.pageAction.setIcon({tabId: tab.id, path: "icons/padlock_red.svg"});
     browser.pageAction.setTitle({tabId: tab.id, title: TITLE_APPLY});
     browser.pageAction.show(tab.id);
   }
   if (tab.url.startsWith('https:')) {
-    browser.pageAction.setIcon({tabId: tab.id, path: "icons/padlock_red.svg"});
+    browser.pageAction.setIcon({tabId: tab.id, path: "icons/padlock_green.svg"});
     browser.pageAction.setTitle({tabId: tab.id, title: TITLE_REMOVE});
     browser.pageAction.show(tab.id);
   }
